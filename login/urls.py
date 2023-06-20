df8b05959af969eb274813011fba355b724ee5b5
Sara Cobaleda <Sara.CobaledaC@gmail.com>
2023-06-19 23:09:34 -0500

    continuacion de la practica para enviar parametros, y creacion usuario admin

--- login/urls.py
+++ login/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("about/", views.hola),
-    path("hello/<str:username>", views.holaMundo)
+    path("hello/<str:username>", views.holaMundo),
+    path("projects/", views.projects),
+    path("tasks/<str:name>", views.task),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -11,3 +13,11 @@ def holaMundo(request, username):
 
 def hola(request):
     return HttpResponse("<h2>holi</h2>")
+
+def projects(request):
+    project = list(Project.objects.values())
+    return JsonResponse(project, safe = False)
+
+def task(request, name):
+    task = get_object_or_404(Project, name=name)
+    return HttpResponse("task: %s" % task.id)
