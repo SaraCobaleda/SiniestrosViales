8c71ea4d592c107afebba12a34813b987d98c322
Sara Cobaleda <Sara.CobaledaC@gmail.com>
2023-06-29 00:58:46 -0500

    avance en el diagnostico de la calidad de los datos

--- ProcesamientoDatos/Limpieza.py
+++ ProcesamientoDatos/Limpieza.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import jaccard_score
 
 siniestros = pd.read_excel(r"D:\SiniestrosViales\ProcesamientoDatos\siniestros_viales_consolidados_bogota_dc.xlsx", sheet_name="SINIESTROS")
 actorVial = pd.read_excel(r"D:\SiniestrosViales\ProcesamientoDatos\siniestros_viales_consolidados_bogota_dc.xlsx", sheet_name="ACTOR_VIAL")
@@ -11,43 +12,32 @@ historico = pd.read_csv(r"D:\SiniestrosViales\ProcesamientoDatos\historico_sinie
 
 #DIAGNOSTICO
 
-#Join
+#Left Join
 accidentes = pd.merge(siniestros, actorVial, on='CODIGO_ACCIDENTE', how='left')
 accidentes = pd.merge(accidentes, vehiculos, on='CODIGO_ACCIDENTE', how='left')
 accidentes = pd.merge(accidentes, hipotesis, on='CODIGO_ACCIDENTE', how='left')
 
-#actorVial
-print(accidentes.columns)
-print(accidentes.dtypes)
+columnas  = (['CODIGO_ACCIDENTE', 'FECHA_x', 'HORA', 'GRAVEDAD', 'CLASE_x', 'CHOQUE',
+              'OBJETO_FIJO', 'DIRECCION', 'CODIGO_LOCALIDAD', 'DISENO_LUGAR',
+              'CODIGO_ACCIDENTADO', 'FECHA_y', 'CONDICION', 'ESTADO', 'EDAD', 'SEXO',
+              'VEHICULO_x', 'FECHA_z', 'VEHICULO_y', 'CLASE_y', 'SERVICIO',
+              'MODALIDAD', 'ENFUGA', 'FECHA_q', 'CODIGO_CAUSA'])
 
-print(accidentes['FECHA_x'], accidentes['FECHA_y'])
-
-columnas  = accidentes.columns
+accidentes.columns = columnas
 
 for i in range(accidentes.shape[1]):
-    print("columna hola")
-    print(accidentes[columnas[i]].isna().sum())
+    print(columnas[i], ": ", accidentes[columnas[i]].value_counts())
 
-print(accidentes['CODIGO_ACCIDENTE'].value_counts())
-print(accidentes['FECHA'].value_counts())
-print(accidentes['HORA'].value_counts())
-print(accidentes['GRAVEDAD'].value_counts())
-print(siniestros['CLASE'].value_counts())
-print(accidentes['CHOQUE'].value_counts())
-print(accidentes['OBJETO_FIJO'].value_counts())
-print(accidentes['DIRECCION'].value_counts())
-print(accidentes['CODIGO_LOCALIDAD'].value_counts())
-print(accidentes['DISENO_LUGAR'].value_counts())
+for i in range(accidentes.shape[1]):
+    print(columnas[i], ": ", accidentes[columnas[i]].isna().sum())
 
-print('CODIGO_ACCIDENTE: ', accidentes['CODIGO_ACCIDENTE'].isna().sum())
-print('FECHA: ', accidentes['FECHA'].isna().sum())
-print('HORA: ', accidentes['HORA'].isna().sum())
-print('GRAVEDAD: ', accidentes['GRAVEDAD'].isna().sum())
-print('CLASE: ', accidentes['CLASE'].isna().sum())
-print('CHOQUE: ', accidentes['CHOQUE'].isna().sum())
-print('OBJETO_FIJO: ', accidentes['OBJETO_FIJO'].isna().sum())
-print('DIRECCION: ', accidentes['DIRECCION'].isna().sum())
-print('CODIGO_LOCALIDAD: ', accidentes['CODIGO_LOCALIDAD'].isna().sum())
-print('DISENO_LUGAR: ', accidentes['DISENO_LUGAR'].isna().sum())
+#Comparar la relacion entre colimnas
+conjunto1 = set(accidentes['FECHA_x'])
+conjunto2 = set(accidentes['FECHA_y'])
 
-print(accidentes['CODIGO_ACCIDENTE'].is_unique)
+coeficiente_jaccard = jaccard_score(conjunto1, conjunto2) * 100
+print("Porcentaje de similitud:", coeficiente_jaccard)
+
+correlacion = accidentes['FECHA_x'].corr(accidentes['FECHA_y'])
+porcentaje_similitud = abs(correlacion) * 100
+print("Porcentaje de similitud:", porcentaje_similitud)
