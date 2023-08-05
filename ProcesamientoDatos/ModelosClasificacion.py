05b9b5965bb51d121845486e48ddb421fc7740e5
Sara Cobaleda <Sara.CobaledaC@gmail.com>
2023-08-04 23:47:18 -0500

    avance con las graficas

--- ProcesamientoDatos/ModelosClasificacion.py
+++ ProcesamientoDatos/ModelosClasificacion.py
@@ -16,14 +16,16 @@ df = pd.read_csv(r"D:\SiniestrosViales\ProcesamientoDatos\DataLimpia.csv")
 df = df.drop('FECHA_HORA', axis=1)
 
 #muestra aleatoria
-muestra = 10000
+muestra = 1000
 df = df.sample(n=muestra)
 
 # Cuantos Valores faltantes hay
 missing_values = df.isnull().sum()
 print(missing_values)
+
+
     
-#APLICANDO ALGORITMOS
+#APLICANDO ALGORITMOS GRAVEDAD
 df = df.apply(pd.to_numeric, errors='coerce')
 
 for i in range(df.shape[1]):
@@ -38,7 +40,6 @@ X = X.values
 
 # Dividir los datos en conjuntos de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 # SVM
 # Crear un modelo SVM
@@ -55,11 +56,10 @@ accuracy = accuracy_score(y_test, y_pred)
 print("Precisión del modelo SVM:", accuracy)
 
 #Exportamos el modelo
-joblib.dump(svm_model, r'D:\SiniestrosViales\Modelos Clasificacion\svm_model.pkl')
+joblib.dump(svm_model, r'D:\SiniestrosViales\Modelos Clasificacion\svm_model_GRAVEDAD.pkl')
 
 # Cargar el modelo desde el archivo
 #svm_model = joblib.load('svm_model.pkl')
-
 
 # K-Nearest Neighbors
 # Crear un modelo KNN con K=3 (3 vecinos más cercanos)
@@ -81,7 +81,6 @@ joblib.dump(knn_model, r'D:\SiniestrosViales\Modelos Clasificacion\knn_model_GRA
 # Cargar el modelo desde el archivo
 #knn_model = joblib.load('knn_model.pkl')
 
-
 # RandomForestClassifier
 # Crear un modelo Random Forest con 100 árboles
 rf_model = RandomForestClassifier(n_estimators=100)
@@ -101,11 +100,153 @@ joblib.dump(rf_model, r'D:\SiniestrosViales\Modelos Clasificacion\rf_model_GRAVE
 
 
 
+#APLICANDO ALGORITMOS CLASE_SINIESTRO
+df = df.apply(pd.to_numeric, errors='coerce')
+
+for i in range(df.shape[1]):
+    df[df.columns[i]] = df[df.columns[i]].fillna(0)
+
+X = df.drop('CLASE_SINIESTRO', axis=1)
+y = df['CLASE_SINIESTRO']
+
+#pasarlos a arreglos de numpy
+y = y.values
+X = X.values
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# SVM
+# Crear un modelo SVM
+svm_model = svm.SVC(kernel='linear')
+
+# Entrenar el modelo SVM con los datos de entrenamiento
+svm_model.fit(X_train, y_train)
+
+# Predecir las clases para los datos de prueba
+y_pred = svm_model.predict(X_test)
+
+# Calcular la precisión (accuracy) del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("Precisión del modelo SVM:", accuracy)
+
+#Exportamos el modelo
+joblib.dump(svm_model, r'D:\SiniestrosViales\Modelos Clasificacion\svm_model_CLASE_SINIESTRO.pkl')
+
+# Cargar el modelo desde el archivo
+#svm_model = joblib.load('svm_model.pkl')
+
+# K-Nearest Neighbors
+# Crear un modelo KNN con K=3 (3 vecinos más cercanos)
+knn_model = KNeighborsClassifier(n_neighbors=3)
+
+# Entrenar el modelo KNN con los datos de entrenamiento
+knn_model.fit(X_train, y_train)
+
+# Predecir las clases para los datos de prueba
+y_pred = knn_model.predict(X_test)
+
+# Calcular la precisión (accuracy) del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("Precisión del modelo KNN:", accuracy)
+
+#Exportamos el modelo
+joblib.dump(knn_model, r'D:\SiniestrosViales\Modelos Clasificacion\knn_model_CLASE_SINIESTRO.pkl')
+
+# Cargar el modelo desde el archivo
+#knn_model = joblib.load('knn_model.pkl')
+
+# RandomForestClassifier
+# Crear un modelo Random Forest con 100 árboles
+rf_model = RandomForestClassifier(n_estimators=100)
+
+# Entrenar el modelo Random Forest con los datos de entrenamiento
+rf_model.fit(X_train, y_train)
+
+# Predecir las clases para los datos de prueba
+y_pred = rf_model.predict(X_test)
+
+# Calcular la precisión (accuracy) del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("Precisión del modelo Random Forest:", accuracy)
+
+#Exportamos el modelo
+joblib.dump(rf_model, r'D:\SiniestrosViales\Modelos Clasificacion\rf_model_CLASE_SINIESTRO.pkl')
 
 
 
+#APLICANDO ALGORITMOS CODIGO_CAUSA
+df = df.apply(pd.to_numeric, errors='coerce')
 
+for i in range(df.shape[1]):
+    df[df.columns[i]] = df[df.columns[i]].fillna(0)
 
+X = df.drop('CODIGO_CAUSA', axis=1)
+y = df['CODIGO_CAUSA']
+
+#pasarlos a arreglos de numpy
+y = y.values
+X = X.values
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# SVM
+# Crear un modelo SVM
+svm_model = svm.SVC(kernel='linear')
+
+# Entrenar el modelo SVM con los datos de entrenamiento
+svm_model.fit(X_train, y_train)
+
+# Predecir las clases para los datos de prueba
+y_pred = svm_model.predict(X_test)
+
+# Calcular la precisión (accuracy) del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("Precisión del modelo SVM:", accuracy)
+
+#Exportamos el modelo
+joblib.dump(svm_model, r'D:\SiniestrosViales\Modelos Clasificacion\svm_model_CODIGO_CAUSA.pkl')
+
+# Cargar el modelo desde el archivo
+#svm_model = joblib.load('svm_model.pkl')
+
+# K-Nearest Neighbors
+# Crear un modelo KNN con K=3 (3 vecinos más cercanos)
+knn_model = KNeighborsClassifier(n_neighbors=3)
+
+# Entrenar el modelo KNN con los datos de entrenamiento
+knn_model.fit(X_train, y_train)
+
+# Predecir las clases para los datos de prueba
+y_pred = knn_model.predict(X_test)
+
+# Calcular la precisión (accuracy) del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("Precisión del modelo KNN:", accuracy)
+
+#Exportamos el modelo
+joblib.dump(knn_model, r'D:\SiniestrosViales\Modelos Clasificacion\knn_model_CODIGO_CAUSA.pkl')
+
+# Cargar el modelo desde el archivo
+#knn_model = joblib.load('knn_model.pkl')
+
+# RandomForestClassifier
+# Crear un modelo Random Forest con 100 árboles
+rf_model = RandomForestClassifier(n_estimators=100)
+
+# Entrenar el modelo Random Forest con los datos de entrenamiento
+rf_model.fit(X_train, y_train)
+
+# Predecir las clases para los datos de prueba
+y_pred = rf_model.predict(X_test)
+
+# Calcular la precisión (accuracy) del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("Precisión del modelo Random Forest:", accuracy)
+
+#Exportamos el modelo
+joblib.dump(rf_model, r'D:\SiniestrosViales\Modelos Clasificacion\rf_model_CODIGO_CAUSA.pkl')
 
 
 
--- analiticaSiniestros/views.py
+++ analiticaSiniestros/views.py
@@ -23,13 +23,54 @@ def index(request):
     claseSiniestro = [registro.claseSiniestro for registro in registros_mas_recientes]
     claseSiniestro = np.array(list(claseSiniestro))
 
-    df = pd.DataFrame({'Fecha': fechaHora, 'Valor': gravedad})
-    fig = px.line(df, x='Fecha', y='Valor', markers=True)
+    choque = [registro.choque for registro in registros_mas_recientes]
+    choque = np.array(list(claseSiniestro))
+
+    codigoLocalidad = [registro.codigoLocalidad for registro in registros_mas_recientes]
+    codigoLocalidad = np.array(list(claseSiniestro))
+
+    disenoLugar = [registro.disenoLugar for registro in registros_mas_recientes]
+    disenoLugar = np.array(list(claseSiniestro))
+
+    condicion = [registro.condicion for registro in registros_mas_recientes]
+    condicion = np.array(list(claseSiniestro))
+
+    estado = [registro.estado for registro in registros_mas_recientes]
+    estado = np.array(list(claseSiniestro))
+
+    edad = [registro.edad for registro in registros_mas_recientes]
+    edad = np.array(list(claseSiniestro))
+
+    sexo = [registro.sexo for registro in registros_mas_recientes]
+    sexo = np.array(list(claseSiniestro))
+
+    claseVehiculo = [registro.claseVehiculo for registro in registros_mas_recientes]
+    claseVehiculo = np.array(list(claseSiniestro))
+
+    servicio = [registro.servicio for registro in registros_mas_recientes]
+    servicio = np.array(list(claseSiniestro))
+
+    enfuga = [registro.enfuga for registro in registros_mas_recientes]
+    enfuga = np.array(list(claseSiniestro))
+
+    codigoCausa = [registro.codigoCausa for registro in registros_mas_recientes]
+    codigoCausa = np.array(list(claseSiniestro))
+
+    dfFecha = pd.DataFrame({'Fecha': fechaHora})
+    df = pd.DataFrame({'Fecha': fechaHora, 'gravedad': gravedad, "choque":choque, "condicion":condicion})
+    fig = px.line(df, x='Fecha', y='gravedad', markers=True)
     plot_div = fig.to_html(full_html=False, include_plotlyjs=True)
+
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=fechaHora, y=choque ,mode='lines',name='lines'))
+    fig2.add_trace(go.Scatter(x=fechaHora, y=gravedad ,mode='markers+markers',name='lines+markers'))
+    fig2.add_trace(go.Scatter(x=fechaHora, y=condicion,mode='markers', name='markers'))
+
+    plot_div2 = fig2.to_html(full_html=False, include_plotlyjs=True)
 
     datos  = Siniestro.objects.all()
     print(datos)
-    return render(request, 'index.html', {'plot_div': plot_div})
+    return render(request, 'index.html', {'plot_div': plot_div, 'plot_div2': plot_div2})
 
 @login_required
 def usersProfile(request):
