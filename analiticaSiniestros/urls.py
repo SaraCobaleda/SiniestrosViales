36010188f1f8a3fad81940e9a93229a40be33cb0
Sara Cobaleda <Sara.CobaledaC@gmail.com>
2023-08-25 00:53:57 -0500

    Agregando la funcionalidad de crear registro

--- analiticaSiniestros/urls.py
+++ analiticaSiniestros/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("usersProfile", views.usersProfile, name = 'usersProfile'),
     path("basicQuestion", views.basicQuestions, name = 'basicQuestions'),
     path("diccionario", views.diccionario, name = 'diccionario'),
-    path("verDatos", views.verDatos, name = 'verDatos')
+    path("verDatos", views.verDatos, name = 'verDatos'),
+    path("crearDatos", views.crearDatos, name = 'crearDatos'),
+    path("modificarDatos", views.modificarDatos, name = 'modificarDatos'),
+    path("eliminarDatos", views.eliminarDatos, name = 'eliminarDatos')
 ]
--- analiticaSiniestros/views.py
+++ analiticaSiniestros/views.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 # Create your views here.
 
+
 @login_required
 def index(request):
     registros_mas_recientes = Siniestro.objects.order_by('-fechaHora')[:100]
@@ -22,16 +23,19 @@ def index(request):
     gravedad = [registro.gravedad for registro in registros_mas_recientes]
     gravedad = np.array(list(gravedad))
 
-    claseSiniestro = [registro.claseSiniestro for registro in registros_mas_recientes]
+    claseSiniestro = [
+        registro.claseSiniestro for registro in registros_mas_recientes]
     claseSiniestro = np.array(list(claseSiniestro))
 
     choque = [registro.choque for registro in registros_mas_recientes]
     choque = np.array(list(claseSiniestro))
 
-    codigoLocalidad = [registro.codigoLocalidad for registro in registros_mas_recientes]
+    codigoLocalidad = [
+        registro.codigoLocalidad for registro in registros_mas_recientes]
     codigoLocalidad = np.array(list(claseSiniestro))
 
-    disenoLugar = [registro.disenoLugar for registro in registros_mas_recientes]
+    disenoLugar = [
+        registro.disenoLugar for registro in registros_mas_recientes]
     disenoLugar = np.array(list(claseSiniestro))
 
     condicion = [registro.condicion for registro in registros_mas_recientes]
@@ -46,7 +50,8 @@ def index(request):
     sexo = [registro.sexo for registro in registros_mas_recientes]
     sexo = np.array(list(claseSiniestro))
 
-    claseVehiculo = [registro.claseVehiculo for registro in registros_mas_recientes]
+    claseVehiculo = [
+        registro.claseVehiculo for registro in registros_mas_recientes]
     claseVehiculo = np.array(list(claseSiniestro))
 
     servicio = [registro.servicio for registro in registros_mas_recientes]
@@ -55,10 +60,12 @@ def index(request):
     enfuga = [registro.enfuga for registro in registros_mas_recientes]
     enfuga = np.array(list(claseSiniestro))
 
-    codigoCausa = [registro.codigoCausa for registro in registros_mas_recientes]
+    codigoCausa = [
+        registro.codigoCausa for registro in registros_mas_recientes]
     codigoCausa = np.array(list(claseSiniestro))
 
-    df = pd.DataFrame({'Fecha': fechaHora, 'gravedad': gravedad, "choque":choque, "condicion":condicion})
+    df = pd.DataFrame({'Fecha': fechaHora, 'gravedad': gravedad,
+                      "choque": choque, "condicion": condicion})
 
     fig = px.scatter(df, x='Fecha', y='gravedad')
     plot_div = fig.to_html(full_html=False, include_plotlyjs=True)
@@ -70,65 +77,120 @@ def index(request):
     mujer = 0
 
     for genero in sexo:
-         if genero > 0:
-              hombre = hombre + 1
-         else:
-              mujer = mujer + 1
+        if genero > 0:
+            hombre = hombre + 1
+        else:
+            mujer = mujer + 1
 
     datos_pastel = {"Hombres": hombre, "Mujeres": mujer}
 
-    piefig = px.pie(values=list(datos_pastel.values()), names=list(datos_pastel.keys()))
+    piefig = px.pie(values=list(datos_pastel.values()),
+                    names=list(datos_pastel.keys()))
     plot_div3 = piefig.to_html(full_html=False, include_plotlyjs=True)
 
     fig4 = px.scatter(df, x='Fecha', y='condicion')
     plot_div4 = fig2.to_html(full_html=False, include_plotlyjs=True)
 
-    datos  = Siniestro.objects.all()
+    datos = Siniestro.objects.all()
     print(datos)
-    return render(request, 'index.html', {'plot_div': plot_div, 
-                                          'plot_div2': plot_div2, 
-                                          'plot_div3': plot_div3, 
+    return render(request, 'index.html', {'plot_div': plot_div,
+                                          'plot_div2': plot_div2,
+                                          'plot_div3': plot_div3,
                                           'plot_div4': plot_div4})
+
 
 @login_required
 def diccionario(request):
-     gravedades = Gravedad.objects.all()
-     claseSinisestro = ClaseSiniestro.objects.all()
-     choque = Choque.objects.all()
-     codigoLocalidad = CodigoLocalidad.objects.all()
-     disenoLugar = DisenoLugar.objects.all()
-     condicion = Condicion.objects.all()
-     estado = Estado.objects.all()
-     edad = Siniestro.objects.values_list('edad', flat=True).distinct()
-     sexo = Sexo.objects.all()
-     claseVehiculo = ClaseVehiculo.objects.all()
-     servicio = Servicio.objects.all()
-     enfuga = Enfuga.objects.all()
-     codigoCausa = CodigoCausa.objects.all()
-     return render(request, 'diccionario.html', {'gravedades': gravedades,
-                                                 'claseSiniestros': claseSinisestro,
-                                                 'choques': choque,
-                                                 'codigoLocalidades': codigoLocalidad,
-                                                 'disenoLugares': disenoLugar,
-                                                 'condiciones': condicion,
-                                                 'estados': estado,
-                                                 'edades': edad,
-                                                 'sexos': sexo,
-                                                 'claseVehiculos': claseVehiculo,
-                                                 'servicios': servicio,
-                                                 'enfugas': enfuga,
-                                                 'codigoCausas': codigoCausa})
+    gravedades = Gravedad.objects.all()
+    claseSinisestro = ClaseSiniestro.objects.all()
+    choque = Choque.objects.all()
+    codigoLocalidad = CodigoLocalidad.objects.all()
+    disenoLugar = DisenoLugar.objects.all()
+    condicion = Condicion.objects.all()
+    estado = Estado.objects.all()
+    edad = Siniestro.objects.values_list('edad', flat=True).distinct()
+    sexo = Sexo.objects.all()
+    claseVehiculo = ClaseVehiculo.objects.all()
+    servicio = Servicio.objects.all()
+    enfuga = Enfuga.objects.all()
+    codigoCausa = CodigoCausa.objects.all()
+    return render(request, 'diccionario.html', {'gravedades': gravedades,
+                                                'claseSiniestros': claseSinisestro,
+                                                'choques': choque,
+                                                'codigoLocalidades': codigoLocalidad,
+                                                'disenoLugares': disenoLugar,
+                                                'condiciones': condicion,
+                                                'estados': estado,
+                                                'edades': edad,
+                                                'sexos': sexo,
+                                                'claseVehiculos': claseVehiculo,
+                                                'servicios': servicio,
+                                                'enfugas': enfuga,
+                                                'codigoCausas': codigoCausa})
+
 
 @login_required
 def verDatos(request):
-     siniestro = Siniestro.objects.all()
-     return render(request, 'ver-datos.html', {'siniestros': siniestro})
+    siniestro = Siniestro.objects.all()
+    return render(request, 'ver-datos.html', {'siniestros': siniestro})
+
 
 @login_required
 def usersProfile(request):
-     return render(request, 'users-profile.html')
+    return render(request, 'users-profile.html')
+
 
 @login_required
 def basicQuestions(request):
-     print("basicQuestions")
-     return render(request, 'pages-faq.html')
+    return render(request, 'pages-faq.html')
+
+
+@login_required
+def crearDatos(request):
+    gravedades = Gravedad.objects.all()
+    claseSinisestro = ClaseSiniestro.objects.all()
+    choque = Choque.objects.all()
+    codigoLocalidad = CodigoLocalidad.objects.all()
+    disenoLugar = DisenoLugar.objects.all()
+    condicion = Condicion.objects.all()
+    estado = Estado.objects.all()
+    sexo = Sexo.objects.all()
+    claseVehiculo = ClaseVehiculo.objects.all()
+    servicio = Servicio.objects.all()
+    codigoCausa = CodigoCausa.objects.all()
+
+    if request.method == 'POST':
+        return render(request, 'crear-datos.html', {'gravedades': gravedades,
+                                                    'claseSiniestros': claseSinisestro,
+                                                    'choques': choque,
+                                                    'codigoLocalidades': codigoLocalidad,
+                                                    'disenoLugares': disenoLugar,
+                                                    'condiciones': condicion,
+                                                    'estados': estado,
+                                                    'sexos': sexo,
+                                                    'claseVehiculos': claseVehiculo,
+                                                    'servicios': servicio,
+                                                    'codigoCausas': codigoCausa})
+
+    else:
+        return render(request, 'crear-datos.html', {'gravedades': gravedades,
+                                                    'claseSiniestros': claseSinisestro,
+                                                    'choques': choque,
+                                                    'codigoLocalidades': codigoLocalidad,
+                                                    'disenoLugares': disenoLugar,
+                                                    'condiciones': condicion,
+                                                    'estados': estado,
+                                                    'sexos': sexo,
+                                                    'claseVehiculos': claseVehiculo,
+                                                    'servicios': servicio,
+                                                    'codigoCausas': codigoCausa})
+
+
+@login_required
+def modificarDatos(request):
+    return render(request, 'actualizar-datos.html')
+
+
+@login_required
+def eliminarDatos(request):
+    return render(request, 'borrar-datos.html')
